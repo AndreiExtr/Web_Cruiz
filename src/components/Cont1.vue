a113b86fb5ff5cdeff0c10aaa41f929781590476
akropinov <andreikropinov17121988@gmail.com>
2024-02-05 23:13:26 +0300

    new vue

---
+++ src/components/Cont1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container_1">
+      <div class="title_button">
+        <h1>Выберите свой <br> вариант путешествия</h1>
+        <button  class="select-cruise-button" >Выбрать круиз</button>
+      </div>
+
+
+      <div class="overlay"></div>
+      <img src="/src/assets/1.jpg" alt="Background Image" class="background-image">
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
