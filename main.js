9457652d6c5b62f7ed2567599267ded154946dc3
akropinov <andreikropinov17121988@gmail.com>
2024-02-05 21:49:12 +0300

    test

--- main.js
+++
@@ -1,5 +0,0 @@
-
-import { createModal } from 'vue';
-import Modal from './src/components/Modal.vue';
-
-createModal(Modal).mount('#modal');
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>Hello {{name}}</div>
+</template>
+<script>
+export default {
+  name: "App",
+  data(){
+    return{
+      name: "Vieee",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+font-family: 'DM Sans', sans-serif;
+  text-align: center;
+  padding: 90px;
+  color: brown;
+  z-index: 3;
+}
+</style>
