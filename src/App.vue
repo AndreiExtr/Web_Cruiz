4e6bad9b92edf84b1fcd7df760ad0540bef95dd1
akropinov <andreikropinov17121988@gmail.com>
2024-02-07 12:15:27 +0300

    slider

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
-<script setup>
-</script>
 
 <template>
 
   <Head />
   <Cont1 />
+  <Cont2 />
 
 </template>
 <script>
 import Head from "@/components/Head.vue";
 import Cont1 from "@/components/Cont1.vue"
+import Cont2 from "@/components/Cont2.vue"
 
 export default {
   name: 'App',
   components: {
     Head,
     Cont1,
+    Cont2,
   }
 }
 </script>
